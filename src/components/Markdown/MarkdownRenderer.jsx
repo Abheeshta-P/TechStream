aa07ae9b0c9d00f31fda2859c2a8d34fc9b64146
Abheeshta-P <softwarecheck01+github@gmail.com>
2024-09-24 18:53:34 +0530

    syyntax highlighting markdown

--- src/components/Markdown/MarkdownRenderer.jsx
+++ src/components/Markdown/MarkdownRenderer.jsx
@@ -2,20 +2,39 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism'; // You can choose different themes here
 
 const MarkdownRenderer = ({ content }) => {
   return (
     <div className="browser-css text-base p-5 w-[98%] prose ">
-      <ReactMarkdown remarkPlugins={[remarkGfm]} rehypePlugins={[rehypeRaw]}>
-      {content}
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw]}
+        components={{
+          code({ inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match ? (
+              <SyntaxHighlighter
+                style={oneDark} // You can change the theme here
+                language={match[1]}
+                PreTag="div"
+                {...props}
+              >
+                {String(children).replace(/\n$/, '')}
+              </SyntaxHighlighter>
+            ) : (
+              <code className={className} {...props}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {content}
       </ReactMarkdown>
     </div>
   );
 };
-// Example usage
-const post = {
-  content: '# Heading\n\nhi new'
-};
 
-
-export default  MarkdownRenderer;
+export default MarkdownRenderer;
